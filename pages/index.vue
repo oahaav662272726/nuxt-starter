e902b5f5e6879e7cc4bcb434815244209f218b24
oahaav662272726 <oahaav662272726@gmail.com>
2025-07-18 15:51:55 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,36 @@
 <script setup>
-import { onMounted } from 'vue'
+import pageData from '@/data/index.json'
+const page = ref(pageData)
 
-onMounted(() => {
-  window.location.href = 'https://outro-site.com' // substitua pela URL desejada
-})
+definePageMeta({
+  layout: "default",
+});
 </script>
 
 <template>
-  <div></div> <!-- pode deixar vazio já que vai redirecionar -->
+  <Container>
+    <Hero
+      :title="page.hero.title"
+      :description="page.hero.description"
+      :image="page.hero.image"
+      :imageAlt="page.hero.imageAlt"
+      :buttons="page.hero.buttons"
+    ></Hero>
+    <Logos :title="page.logos.title" :icons="page.logos.icons"></Logos>
+    <Features
+      :title="page.features.title"
+      :description="page.features.description"
+      :items="page.features.items"
+    ></Features>
+    <Testimonials
+      :title="page.testimonials.title"
+      :description="page.testimonials.description"
+      :items="page.testimonials.items"
+    ></Testimonials>
+    <Cta
+      :title="page.cta.title"
+      :description="page.cta.description"
+      :buttons="page.cta.buttons"
+    ></Cta>
+  </Container>
 </template>
